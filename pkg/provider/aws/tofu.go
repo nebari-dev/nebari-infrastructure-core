4660123706f42f338d36fb5a57d0a4749e1caa30
Marcelo Villa <36754005+marcelovilla@users.noreply.github.com>
2026-01-28 11:01:22 +0100

    Add TFVars struct and method to convert AWS config to TFVars type

---
+++ pkg/provider/aws/tofu.go
@@ -0,0 +1,96 @@
+package aws
+
+import "embed"
+
+// Embed all files in the templates directory, including dotfiles (i.e. .terraform.lock.hcl)
+//
+//go:embed all:templates
+var tofuTemplates embed.FS
+
+type TFVars struct {
+	Region                        string               `json:"region"`
+	ProjectName                   string               `json:"project_name,omitempty"`
+	Tags                          map[string]string    `json:"tags,omitempty"`
+	AvailabilityZones             []string             `json:"availability_zones,omitempty"`
+	CreateVPC                     bool                 `json:"create_vpc"`
+	VPCCIDRBlock                  *string              `json:"vpc_cidr_block,omitempty"`
+	ExistingVPCID                 *string              `json:"existing_vpc_id,omitempty"`
+	ExistingPrivateSubnetIDs      []string             `json:"existing_private_subnet_ids,omitempty"`
+	CreateSecurityGroup           bool                 `json:"create_security_group"`
+	ExistingSecurityGroupID       *string              `json:"existing_security_group_id,omitempty"`
+	KubernetesVersion             string               `json:"kubernetes_version"`
+	EndpointPrivateAccess         bool                 `json:"endpoint_private_access"`
+	EndpointPublicAccess          bool                 `json:"endpoint_public_access"`
+	EKSKMSArn                     *string              `json:"eks_kms_arn,omitempty"`
+	ClusterEnabledLogTypes        []string             `json:"cluster_enabled_log_types,omitempty"`
+	CreateIAMRoles                bool                 `json:"create_iam_roles"`
+	ExistingClusterIAMRoleArn     *string              `json:"existing_cluster_iam_role_arn,omitempty"`
+	ExistingNodeIAMRoleArn        *string              `json:"existing_node_iam_role_arn,omitempty"`
+	IAMRolePermissionsBoundary    *string              `json:"iam_role_permissions_boundary,omitempty"`
+	NodeGroups                    map[string]NodeGroup `json:"node_groups"`
+	EFSEnabled                    bool                 `json:"efs_enabled"`
+	EFSPerformanceMode            string               `json:"efs_performance_mode,omitempty"`
+	EFSThroughputMode             string               `json:"efs_throughput_mode,omitempty"`
+	EFSProvisionedThroughputMibps *int                 `json:"efs_provisioned_throughput_in_mibps,omitempty"`
+	EFSEncrypted                  bool                 `json:"efs_encrypted"`
+	EFSKMSKeyArn                  *string              `json:"efs_kms_key_arn,omitempty"`
+}
+
+func (c *Config) toTFVars(projectName string) TFVars {
+	vars := TFVars{
+		Region:                 c.Region,
+		ProjectName:            projectName,
+		Tags:                   c.Tags,
+		AvailabilityZones:      c.AvailabilityZones,
+		CreateVPC:              c.ExistingVPCID == "" && len(c.ExistingPrivateSubnetIDs) == 0,
+		CreateSecurityGroup:    c.ExistingSecurityGroupID == "",
+		KubernetesVersion:      c.KubernetesVersion,
+		EndpointPrivateAccess:  c.EndpointPrivateAccess,
+		EndpointPublicAccess:   c.EndpointPublicAccess,
+		ClusterEnabledLogTypes: c.EnabledLogTypes,
+		CreateIAMRoles:         c.ExistingClusterRoleArn == "" && c.ExistingNodeRoleArn == "",
+		NodeGroups:             c.NodeGroups,
+	}
+
+	// Set pointer fields only when values are provided, so omitempty excludes them from JSON.
+	// This lets Terraform use its defaults instead of receiving empty strings.
+	if c.VPCCIDRBlock != "" {
+		vars.VPCCIDRBlock = &c.VPCCIDRBlock
+	}
+	if c.ExistingVPCID != "" {
+		vars.ExistingVPCID = &c.ExistingVPCID
+	}
+	if len(c.ExistingPrivateSubnetIDs) > 0 {
+		vars.ExistingPrivateSubnetIDs = c.ExistingPrivateSubnetIDs
+	}
+	if c.ExistingSecurityGroupID != "" {
+		vars.ExistingSecurityGroupID = &c.ExistingSecurityGroupID
+	}
+	if c.EKSKMSArn != "" {
+		vars.EKSKMSArn = &c.EKSKMSArn
+	}
+	if c.ExistingClusterRoleArn != "" {
+		vars.ExistingClusterIAMRoleArn = &c.ExistingClusterRoleArn
+	}
+	if c.ExistingNodeRoleArn != "" {
+		vars.ExistingNodeIAMRoleArn = &c.ExistingNodeRoleArn
+	}
+	if c.PermissionsBoundary != "" {
+		vars.IAMRolePermissionsBoundary = &c.PermissionsBoundary
+	}
+
+	if c.EFS != nil {
+		vars.EFSEnabled = c.EFS.Enabled
+		vars.EFSPerformanceMode = c.EFS.PerformanceMode
+		vars.EFSThroughputMode = c.EFS.ThroughputMode
+		vars.EFSEncrypted = c.EFS.Encrypted
+		if c.EFS.ProvisionedThroughput > 0 {
+			vars.EFSProvisionedThroughputMibps = &c.EFS.ProvisionedThroughput
+		}
+		if c.EFS.KMSKeyArn != "" {
+			vars.EFSKMSKeyArn = &c.EFS.KMSKeyArn
+		}
+	}
+
+	return vars
+}
