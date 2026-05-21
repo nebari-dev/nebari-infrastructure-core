31676627e75e3c59d5accd19d6626d34f704d2f6
Tyler Potts <49161327+tylerpotts@users.noreply.github.com>
2026-05-21 11:43:33 -0500

    feat(azure): expand Config struct with full schema

--- pkg/provider/azure/config.go
+++ pkg/provider/azure/config.go
@@ -1,35 +1,39 @@
 package azure
 
-// Config represents Azure-specific configuration
+// Config is the user-facing Azure cluster configuration as parsed from the
+// `cluster.azure:` block of NIC YAML.
 type Config struct {
-	Region                  string               `yaml:"region"`
-	KubernetesVersion       string               `yaml:"kubernetes_version,omitempty"`
-	StorageAccountPostfix   string               `yaml:"storage_account_postfix"`
-	AuthorizedIPRanges      []string             `yaml:"authorized_ip_ranges,omitempty"`
-	ResourceGroupName       string               `yaml:"resource_group_name,omitempty"`
-	NodeResourceGroupName   string               `yaml:"node_resource_group_name,omitempty"`
-	NodeGroups              map[string]NodeGroup `yaml:"node_groups,omitempty"`
-	VnetSubnetID            string               `yaml:"vnet_subnet_id,omitempty"`
-	PrivateClusterEnabled   bool                 `yaml:"private_cluster_enabled,omitempty"`
-	Tags                    map[string]string    `yaml:"tags,omitempty"`
-	NetworkProfile          map[string]string    `yaml:"network_profile,omitempty"`
-	MaxPods                 int                  `yaml:"max_pods,omitempty"`
-	WorkloadIdentityEnabled bool                 `yaml:"workload_identity_enabled,omitempty"`
-	AzurePolicyEnabled      bool                 `yaml:"azure_policy_enabled,omitempty"`
-	AdditionalFields        map[string]any       `yaml:",inline"`
+	Region                string               `yaml:"region"`
+	ResourceGroupName     string               `yaml:"resource_group_name,omitempty"`
+	CreateResourceGroup   *bool                `yaml:"create_resource_group,omitempty"`
+	KubernetesVersion     string               `yaml:"kubernetes_version,omitempty"`
+	SKUTier               string               `yaml:"sku_tier,omitempty"`
+	PrivateClusterEnabled bool                 `yaml:"private_cluster_enabled,omitempty"`
+	AuthorizedIPRanges    []string             `yaml:"authorized_ip_ranges,omitempty"`
+	Network               *NetworkConfig       `yaml:"network,omitempty"`
+	NodeGroups            map[string]NodeGroup `yaml:"node_groups"`
+	Tags                  map[string]string    `yaml:"tags,omitempty"`
 }
 
-// NodeGroup represents Azure-specific node group configuration
+// NetworkConfig groups all VNet/subnet/CIDR knobs.
+type NetworkConfig struct {
+	VNetCIDRBlock        string `yaml:"vnet_cidr_block,omitempty"`
+	NodeSubnetCIDRBlock  string `yaml:"node_subnet_cidr_block,omitempty"`
+	PodCIDR              string `yaml:"pod_cidr,omitempty"`
+	ServiceCIDR          string `yaml:"service_cidr,omitempty"`
+	DNSServiceIP         string `yaml:"dns_service_ip,omitempty"`
+	ExistingVNetID       string `yaml:"existing_vnet_id,omitempty"`
+	ExistingNodeSubnetID string `yaml:"existing_node_subnet_id,omitempty"`
+}
+
+// NodeGroup describes one AKS node pool.
 type NodeGroup struct {
-	Instance string  `yaml:"instance"`
-	MinNodes int     `yaml:"min_nodes,omitempty"`
-	MaxNodes int     `yaml:"max_nodes,omitempty"`
-	Taints   []Taint `yaml:"taints,omitempty"`
-}
-
-// Taint represents a Kubernetes taint
-type Taint struct {
-	Key    string `yaml:"key"`
-	Value  string `yaml:"value"`
-	Effect string `yaml:"effect"` // NoSchedule, PreferNoSchedule, NoExecute
+	Instance     string            `yaml:"instance"`
+	MinNodes     int               `yaml:"min_nodes"`
+	MaxNodes     int               `yaml:"max_nodes"`
+	Mode         string            `yaml:"mode,omitempty"` // "System" | "User"; defaults to "User"
+	OSDiskSizeGB int               `yaml:"os_disk_size_gb,omitempty"`
+	Labels       map[string]string `yaml:"labels,omitempty"`
+	Taints       []string          `yaml:"taints,omitempty"`
+	Zones        []string          `yaml:"zones,omitempty"`
 }
