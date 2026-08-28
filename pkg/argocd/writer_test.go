e9ea18c8d4f45a78fdf7f53289c6ba1f46a8fb67
Marcelo Villa <36754005+marcelovilla@users.noreply.github.com>
2026-08-28 16:05:30 +0200

    Add crossplane pkg to test

--- pkg/argocd/writer_test.go
+++ pkg/argocd/writer_test.go
@@ -1240,6 +1240,7 @@ var helmValueFilesApps = []struct {
 	{"opentelemetry-collector", "repository: otel/opentelemetry-collector-k8s"},
 	{"keycloak", "name: KEYCLOAK_ADMIN"},
 	{"nebari-landingpage", "existingSecret: \"nebari-landing-redis\""},
+	{"crossplane", "xpkg.upbound.io/crossplane-contrib/function-auto-ready"},
 }
 
 // seamTemplateData returns TemplateData populated enough that every Helm
